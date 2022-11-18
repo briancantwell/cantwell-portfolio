dbe4cee58db8d0b1ad88bbfc49e5443583ea7e12
llama-anteater <cantwelb@uci.edu>
2022-11-18 14:54:05 -0800

    changed soundcloud logo modified ProjectSingle cards

--- src/components/projects/ProjectSingle.vue
+++ src/components/projects/ProjectSingle.vue
@@ -8,20 +8,23 @@ export default {
 </script>
 
 <template>
-	<!-- <h2>Under Construction :-(</h2> -->
 	<router-link :to="'/projects/' + project.url"
 		class="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark"
 		aria-label="Single Project">
-		<div>
-			<img :src="project.logourl" :alt="project.title" class="rounded-t-xl border-none mx-auto p-2" />
+		<div class="grid overflow-hidden grid-lines grid-cols-1 auto-rows-auto gap-2.5 gap-y-0">
+			<div class="text-center px-4 py-6">
+				<p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2">
+					{{ project.title }}
+				</p>
+				<span class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light">{{ project.category
+				}}</span>
+			</div>
+			<div>
+				<img :src="project.logourl" :alt="project.title" class="rounded-2xl border-none m-auto p-2" />
+			</div>
+
 		</div>
-		<div class="text-center px-4 py-6">
-			<p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2">
-				{{ project.title }}
-			</p>
-			<span class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light">{{ project.category
-			}}</span>
-		</div>
+
 	</router-link>
 </template>
 
