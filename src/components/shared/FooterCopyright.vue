72da3dece4992fd624298e6847ccb10fdf0a2fd1
llama-anteater <cantwelb@uci.edu>
2022-11-19 11:42:59 -0800

    updated github link

--- src/components/shared/FooterCopyright.vue
+++ src/components/shared/FooterCopyright.vue
@@ -25,7 +25,7 @@ export default {
 			</a>
 			Developed by
 			<a
-				href="https://github.com/llama-anteater/"
+				href="https://github.com/briancantwell/"
 				target="__blank"
 				class="font-general-medium text-secondary-dark dark:text-secondary-light uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 duration-500"
 				>{{ author }}</a
